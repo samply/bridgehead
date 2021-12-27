3bece857178f07b8a55b55e184d716cfe61162d3
Patrick Skowronek <patrick.skowronek@dkfz-heidelberg.de>
2021-12-27 13:24:17 +0100

    Added start and stop bridgehead scripts

---
+++ start-bridgehead.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+### Note: Currently not complete, needs some features before useable for production
+
+./prerequisites.sh
+source site.conf
+
+echo "Starting bridgehead"
+
+cd ${project}
+
+docker-compose --env-file ../site-config/${project}.env up
+
+cd ..
+
+echo "The bridgehead should be in online in a few seconds"
---
+++ stop-bridgehead.sh
@@ -0,0 +1,9 @@
+echo "Stoping bridgehead"
+
+source site.conf
+
+cd ${project}
+
+docker-compose --env-file ../site-config/{project}.env down
+
+cd ..

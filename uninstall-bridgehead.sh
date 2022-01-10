bcf1ba2ce14723f3b415674ab83c4518f77a2671
Martin Lablans <m.lablans@dkfz-heidelberg.de>
2022-01-10 16:39:46 +0100

    Make systemctl disable more robust

--- uninstall-bridgehead.sh
+++ uninstall-bridgehead.sh
@@ -5,8 +5,6 @@ source lib/functions.sh
 
 echo "Stopping systemd services and removing bridgehead ..."
 
-systemctl disable --now bridgehead@${project}.service
-systemctl disable --now bridgehead-update@${project}.timer
-systemctl disable --now bridgehead-update@${project}.service
+systemctl disable --now bridgehead@${project}.service bridgehead-update@${project}.timer bridgehead-update@${project}.service
 
 rm -v /etc/systemd/system/{bridgehead\@.service,bridgehead-update\@.timer,bridgehead-update\@.service}

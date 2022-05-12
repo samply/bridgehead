d428f08d8a2824a78acc850710167cb6b579b38f
Martin Lablans <m.lablans@dkfz-heidelberg.de>
2022-05-12 18:23:52 +0200

    Whenever a variable has the value <VAULT>, auto-fetch value from vault.

--- lib/functions.sh
+++ lib/functions.sh
@@ -23,3 +23,21 @@ checkRequirements() {
 		return 0
 	fi
 }
+
+fetchVarsFromVault() {
+	VARS_TO_FETCH=""
+
+	for line in $(cat $@); do
+		if [[ $line =~ .*=\<VAULT\>.* ]]; then
+			VARS_TO_FETCH+="$(echo -n $line | sed 's/=.*//') "
+		fi
+	done
+
+	if [ -z "$VARS_TO_FETCH" ]; then
+		return 0
+	fi
+
+	eval $(docker run --rm -ti -e BW_MASTERPASS -e BW_CLIENTID -e BW_CLIENTSECRET bwcli $VARS_TO_FETCH | sed 's/\r//g')
+
+	return 0
+}

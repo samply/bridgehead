996e8dbd1bb7e6b5c5497916e259592c66bb121b
Torben Brenner <t.brenner@dkfz-heidelberg.de>
2022-09-26 10:54:46 +0200

    refactor: Remove unused Bridgehead Scripts

--- stop-bridgehead.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-source lib/functions.sh
-source site.conf
-
-log "Stopping bridgehead"
-
-docker-compose -f ${project}/docker-compose.yml --env-file site-config/${project}.env down

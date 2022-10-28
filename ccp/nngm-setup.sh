0cba5d315ad4303df8f8393551d4c9afaa026c13
Martin Lablans <m.lablans@dkfz-heidelberg.de>
2022-10-28 10:37:51 +0200

    Sign, not encrypt, to avoid openssl salt

--- ccp/nngm-setup.sh
+++ ccp/nngm-setup.sh
@@ -5,5 +5,5 @@ function nngmSetup() {
 		log INFO "nNGM setup detected -- will start nNGM Connector."
 		OVERRIDE+="-f ./$PROJECT/nngm-compose.yml"
 	fi
-	CONNECTOR_POSTGRES_PASSWORD="$(echo \"This is a salt string to generate one consistent password. It is not required to be secret.\" | openssl rsautl -encrypt -inkey /etc/bridgehead/pki/${SITE_ID}.priv.pem | base64 | head -c 30)"
+	CONNECTOR_POSTGRES_PASSWORD="$(echo \"This is a salt string to generate one consistent password. It is not required to be secret.\" | openssl rsautl -sign -inkey /etc/bridgehead/pki/${SITE_ID}.priv.pem | base64 | head -c 30)"
 }

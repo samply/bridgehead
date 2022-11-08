23aa8f8274704220ac729574ef843ccdfeaa3bf0
Martin Lablans <6804500+lablans@users.noreply.github.com>
2022-11-08 10:54:51 +0100

    Extra space for nngm compose

--- ccp/nngm-setup.sh
+++ ccp/nngm-setup.sh
@@ -3,7 +3,7 @@
 function nngmSetup() {
 	if [ -n "$NNGM_CTS_APIKEY" ]; then
 		log INFO "nNGM setup detected -- will start nNGM Connector."
-		OVERRIDE+="-f ./$PROJECT/nngm-compose.yml"
+		OVERRIDE+=" -f ./$PROJECT/nngm-compose.yml"
 	fi
 	CONNECTOR_POSTGRES_PASSWORD="$(echo \"This is a salt string to generate one consistent password. It is not required to be secret.\" | openssl rsautl -sign -inkey /etc/bridgehead/pki/${SITE_ID}.priv.pem | base64 | head -c 30)"
 }

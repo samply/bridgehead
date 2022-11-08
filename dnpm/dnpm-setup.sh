3a5444dec05eeef7fe4cb31d00058c8a5297f501
Martin Lablans <m.lablans@dkfz-heidelberg.de>
2022-11-08 10:55:18 +0100

    Allow to run DNPM with Connect or with BWHC included

--- dnpm/dnpm-setup.sh
+++ dnpm/dnpm-setup.sh
@@ -2,10 +2,14 @@
 
 function dnpmSetup() {
 	if [ -e /etc/bridgehead/dnpm/local_targets.json ]; then
-		log INFO "DNPM setup detected -- will start DNPM Connector."
-		source /etc/bridgehead/dnpm/shared-but-secret-vars || fail_and_report 1 "Unable to load /etc/bridgehead/dnpm/shared-but-secret-vars"
-		OVERRIDE+="-f ./dnpm/dnpm-compose.yml"
+		log INFO "DNPM setup detected (Beam.Connect) -- will start Beam.Connect for DNPM."
+		OVERRIDE+=" -f ./dnpm/dnpm-compose-beamconnect.yml"
 		DNPM_APPLICATION_SECRET="$(echo \"This is a salt string to generate one consistent password. It is not required to be secret.\" | openssl rsautl -sign -inkey /etc/bridgehead/pki/${SITE_ID}.priv.pem | base64 | head -c 30)"
 		DNPM_BEAM_SECRET_SHORT="$(cat /proc/sys/kernel/random/uuid | sed 's/[-]//g' | head -c 20)"
+		if [ -e /etc/bridgehead/dnpm/bwhcConnectorConfig.xml ]; then
+			log INFO "DNPM setup detected (with Frontend/Backend) -- will start BWHC Frontend/Backend."
+			source /etc/bridgehead/dnpm/shared-but-secret-vars || fail_and_report 1 "Unable to load /etc/bridgehead/dnpm/shared-but-secret-vars"
+			OVERRIDE+=" -f ./dnpm/dnpm-compose-bwhc.yml"
+		fi
 	fi
 }

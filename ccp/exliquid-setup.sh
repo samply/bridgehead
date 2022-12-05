b061bf635039803736066367c154aad8be6c862e
Patrick Skowronek <patrick.skowronek@dkfz-heidelberg.de>
2022-12-05 13:18:14 +0100

    Fixed compose file for exliquid

--- ccp/exliquid-setup.sh
+++ ccp/exliquid-setup.sh
@@ -2,7 +2,7 @@
 
 function exliquidSetup() {
 	if [ -n "$EXLIQUID" ]; then
-		log INFO "EXLIQUID setup detected -- will start Reporthub."
+		log INFO "EXLIQUID setup detected -- will start Report-Hub."
 		OVERRIDE+="-f ./$PROJECT/exliquid-compose.yml"
 	fi
 }

e1e7ffece8899a57ce6c22b1e5f55ebdf083225c
p.delpy@dkfz-heidelberg.de <p.delpy@dkfz-heidelberg.de>
2022-12-16 15:01:04 +0100

    set magicpl instead of patientlist

--- ccp/nngm-setup.sh
+++ ccp/nngm-setup.sh
@@ -12,7 +12,7 @@ function mtbaSetup() {
 	# TODO: Check if ID-Management Module is activated!
 	if [ -n "$ENABLE_MTBA" ];then
 		log INFO "MTBA setup detected -- will start MTBA Service and CBioPortal."
-		if [ -n "$IDMANAGER_UPLOAD_APIKEY" ]; then
+		if [ ! -n "$IDMANAGER_UPLOAD_APIKEY" ]; then
 			log ERROR "Detected MTBA Module configuration but ID-Management Module seems not to be configured!"
 			exit 1;
 		fi

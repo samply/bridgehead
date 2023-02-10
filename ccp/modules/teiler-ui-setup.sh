4bc56fbf33e4d45fbee7f15e4125d4cfe3e97a7d
juarez <d.juarez@dkfz-heidelberg.de>
2023-02-10 12:41:26 +0100

    Add Teiler UI and Teiler module

---
+++ ccp/modules/teiler-ui-setup.sh
@@ -0,0 +1,7 @@
+function teilerUiSetup() {
+	if [ -n "$ENABLE_TEILER" ];then
+		log INFO "Teiler-UI setup detected -- will start Teiler-UI services."
+		OVERRIDE+=" -f ./$PROJECT/modules/teiler-ui-compose.yml"
+	fi
+	KEYCLOAK_DB_PASSWORD="$(echo \"This is a salt string to generate one consistent password. It is not required to be secret.\" | openssl rsautl -sign -inkey /etc/bridgehead/pki/${SITE_ID}.priv.pem | base64 | head -c 30)"
+}

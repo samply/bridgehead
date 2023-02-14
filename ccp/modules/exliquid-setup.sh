969469db04c556c96c259e5eeeff5e526b71188b
p.delpy@dkfz-heidelberg.de <p.delpy@dkfz-heidelberg.de>
2023-02-14 10:20:19 +0100

    beautiful config

--- ccp/modules/exliquid-setup.sh
+++ ccp/modules/exliquid-setup.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-function exliquidSetup() {
-	case ${SITE_ID} in
-		berlin|dresden|essen|frankfurt|freiburg|luebeck|mainz|muenchen-lmu|muenchen-tu|mannheim|tuebingen)
-			EXLIQUID=1
-			;;
-		dktk-test)
-			EXLIQUID=1
-			;;
-		*)
-			EXLIQUID=0
-			;;
-	esac
-	if [[ $EXLIQUID -eq 1 ]]; then
-		log INFO "EXLIQUID setup detected -- will start Report-Hub."
-		OVERRIDE+=" -f ./$PROJECT/modules/exliquid-compose.yml"
-	fi
-}
+case ${SITE_ID} in
+  berlin|dresden|essen|frankfurt|freiburg|luebeck|mainz|muenchen-lmu|muenchen-tu|mannheim|tuebingen)
+    EXLIQUID=1
+    ;;
+  dktk-test)
+    EXLIQUID=1
+    ;;
+  *)
+    EXLIQUID=0
+    ;;
+esac
+if [[ $EXLIQUID -eq 1 ]]; then
+  log INFO "EXLIQUID setup detected -- will start Report-Hub."
+  OVERRIDE+=" -f ./$PROJECT/modules/exliquid-compose.yml"
+fi
--- ccp/modules/nngm-setup.sh
+++ ccp/modules/nngm-setup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-function nngmSetup() {
-	if [ -n "$NNGM_CTS_APIKEY" ]; then
-		log INFO "nNGM setup detected -- will start nNGM Connector."
-		OVERRIDE+=" -f ./$PROJECT/modules/nngm-compose.yml"
-	fi
-	CONNECTOR_POSTGRES_PASSWORD="$(echo \"This is a salt string to generate one consistent password. It is not required to be secret.\" | openssl rsautl -sign -inkey /etc/bridgehead/pki/${SITE_ID}.priv.pem | base64 | head -c 30)"
-}
+if [ -n "$NNGM_CTS_APIKEY" ]; then
+  log INFO "nNGM setup detected -- will start nNGM Connector."
+  OVERRIDE+=" -f ./$PROJECT/modules/nngm-compose.yml"
+fi
+CONNECTOR_POSTGRES_PASSWORD="$(echo \"This is a salt string to generate one consistent password. It is not required to be secret.\" | openssl rsautl -sign -inkey /etc/bridgehead/pki/${SITE_ID}.priv.pem | base64 | head -c 30)"

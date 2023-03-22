6644ae382cb17b3dcb282770023323f1aefed142
juarez <d.juarez@dkfz-heidelberg.de>
2023-03-22 14:41:07 +0100

    Adapt teiler-ui to traefik

---
+++ ccp/modules/login-setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ "$ENABLE_LOGIN" == true ];then
+  log INFO "Login setup detected -- will start Login services."
+  OVERRIDE+=" -f ./$PROJECT/modules/login-compose.yml"
+fi
+KEYCLOAK_DB_PASSWORD="$(echo \"This is a salt string to generate one consistent password. It is not required to be secret.\" | openssl rsautl -sign -inkey /etc/bridgehead/pki/${SITE_ID}.priv.pem | base64 | head -c 30)"

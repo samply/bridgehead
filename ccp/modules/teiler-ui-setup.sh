ec10ca53e0ff69256aa806356595f15a9c99fe0b
juarez <d.juarez@dkfz-heidelberg.de>
2023-03-22 14:41:07 +0100

    Adapt teiler-ui to traefik

--- ccp/modules/teiler-ui-setup.sh
+++ ccp/modules/teiler-ui-setup.sh
@@ -4,4 +4,3 @@ if [ "$ENABLE_TEILER" == true ];then
   log INFO "Teiler-UI setup detected -- will start Teiler-UI services."
   OVERRIDE+=" -f ./$PROJECT/modules/teiler-ui-compose.yml"
 fi
-KEYCLOAK_DB_PASSWORD="$(echo \"This is a salt string to generate one consistent password. It is not required to be secret.\" | openssl rsautl -sign -inkey /etc/bridgehead/pki/${SITE_ID}.priv.pem | base64 | head -c 30)"

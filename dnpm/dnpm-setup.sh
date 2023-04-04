3a42570ac4c6842986c981ca4ec520b5436c2c8f
Tobias Kussel <tobias.kussel@dkfz-heidelberg.de>
2023-04-04 13:11:33 +0200

    Add DNPM discovery URL as public configuration

--- dnpm/dnpm-setup.sh
+++ dnpm/dnpm-setup.sh
@@ -6,10 +6,10 @@ function dnpmSetup() {
 		OVERRIDE+=" -f ./dnpm/dnpm-compose-beamconnect.yml"
 		DNPM_APPLICATION_SECRET="$(echo \"This is a salt string to generate one consistent password. It is not required to be secret.\" | openssl rsautl -sign -inkey /etc/bridgehead/pki/${SITE_ID}.priv.pem | base64 | head -c 30)"
 		DNPM_BEAM_SECRET_SHORT="$(cat /proc/sys/kernel/random/uuid | sed 's/[-]//g' | head -c 20)"
-		source /etc/bridgehead/dnpm/shared-but-secret-vars || fail_and_report 1 "Unable to load /etc/bridgehead/dnpm/shared-but-secret-vars"
+		source /srv/docker/bridgehead/dnpm/vars || fail_and_report 1 "Unable to load /srv/docker/bridgehead/dnpm/vars"
 		export DNPM_DISCOVERY_URL
 		if [ -e /etc/bridgehead/dnpm/bwhcConnectorConfig.xml ]; then
-			log INFO "DNPM setup detected (with Frontend/Backend) -- will start BWHC Frontend/Backend."
+			log INFO "DNPM setup detected (with Frontend/Backend) -- will start BWHC Frontend/Backend. This is highly experimental!"
 			OVERRIDE+=" -f ./dnpm/dnpm-compose-bwhc.yml"
 		fi
 	fi

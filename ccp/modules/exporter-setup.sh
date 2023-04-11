d110fbfe4666ae47b0529a0fb6e346588218fd9d
juarez <d.juarez@dkfz-heidelberg.de>
2023-04-11 11:02:14 +0200

    Change salt string for exporter and login

--- ccp/modules/exporter-setup.sh
+++ ccp/modules/exporter-setup.sh
@@ -5,4 +5,4 @@ if [ -n "$ENABLE_EXPORTER" ];then
   OVERRIDE+=" -f ./$PROJECT/modules/exporter-compose.yml"
 fi
 # TODO: Generate password in another way so that not all passwords are the same?
-EXPORTER_DB_PASSWORD="$(echo \"This is a salt string to generate one consistent password. It is not required to be secret.\" | openssl rsautl -sign -inkey /etc/bridgehead/pki/${SITE_ID}.priv.pem | base64 | head -c 30)"
+EXPORTER_DB_PASSWORD="$(echo \"This is a salt string to generate one consistent password for the exporter. It is not required to be secret.\" | openssl rsautl -sign -inkey /etc/bridgehead/pki/${SITE_ID}.priv.pem | base64 | head -c 30)"

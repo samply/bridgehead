8e998595ce664e3e11a5886fd8a431cd5b9abe7a
juarez <d.juarez@dkfz-heidelberg.de>
2023-05-16 16:40:22 +0200

    Add opal certificate

--- ccp/modules/datashield-setup.sh
+++ ccp/modules/datashield-setup.sh
@@ -5,3 +5,6 @@ if [ "$ENABLE_DATASHIELD" == true ];then
   OVERRIDE+=" -f ./$PROJECT/modules/datashield-compose.yml"
 fi
 OPAL_DB_PASSWORD="$(echo \"This is a salt string to generate one consistent password for Opal. It is not required to be secret.\" | openssl rsautl -sign -inkey /etc/bridgehead/pki/${SITE_ID}.priv.pem | base64 | head -c 30)"
+if [ ! -e "/etc/bridgehead/traefik-tls/opal-cert.pem" ]; then
+  openssl req -x509 -newkey rsa:4096 -nodes -keyout /etc/bridgehead/traefik-tls/opal-key.pem -out /etc/bridgehead/traefik-tls/opal-cert.pem -days 3650 -subj "/CN=$HOST"
+fi

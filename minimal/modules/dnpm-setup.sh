f02587d9fadb6e7349aacbaf64343022624e2dc3
Tobias Kussel <tobias.kussel@dkfz-heidelberg.de>
2023-05-25 11:20:18 +0000

    Change DNPM broker id

--- minimal/modules/dnpm-setup.sh
+++ minimal/modules/dnpm-setup.sh
@@ -7,7 +7,7 @@ if [ -n "${ENABLE_DNPM}" ]; then
 	# Set variables required for Beam-Connect
 	DNPM_APPLICATION_SECRET="$(echo \"This is a salt string to generate one consistent password for DNPM. It is not required to be secret.\" | openssl pkeyutl -sign -inkey /etc/bridgehead/pki/${SITE_ID}.priv.pem | base64 | head -c 30)"
 	DNPM_BEAM_SECRET_SHORT="$(cat /proc/sys/kernel/random/uuid | sed 's/[-]//g' | head -c 20)"
-	DNPM_BROKER_ID="broker.dev.ccp-it.dktk.dkfz.de"
+	DNPM_BROKER_ID="broker.ccp-it.dktk.dkfz.de"
 	DNPM_BROKER_URL="https://${DNPM_BROKER_ID}"
 	DNPM_PROXY_ID="${SITE_ID}.${DNPM_BROKER_ID}"
 fi

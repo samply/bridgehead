09aa33c912eedfb84cd2f4ab5b37e2a3b1d2d576
juarez <d.juarez@dkfz-heidelberg.de>
2023-07-19 13:45:14 +0200

    Generate passwords only if modules are enabled

--- ccp/modules/login-setup.sh
+++ ccp/modules/login-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ "$ENABLE_LOGIN" == true ];then
+if [ "$ENABLE_LOGIN" == true ]; then
   log INFO "Login setup detected -- will start Login services."
   OVERRIDE+=" -f ./$PROJECT/modules/login-compose.yml"
+  KEYCLOAK_DB_PASSWORD="$(echo \"This is a salt string to generate one consistent password for Keycloak. It is not required to be secret.\" | openssl rsautl -sign -inkey /etc/bridgehead/pki/${SITE_ID}.priv.pem | base64 | head -c 30)"
 fi
-KEYCLOAK_DB_PASSWORD="$(echo \"This is a salt string to generate one consistent password for Keycloak. It is not required to be secret.\" | openssl rsautl -sign -inkey /etc/bridgehead/pki/${SITE_ID}.priv.pem | base64 | head -c 30)"

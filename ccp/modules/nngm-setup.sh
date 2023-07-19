8323c5e429fe4105452fac4c3ed0b369cefa6c11
juarez <d.juarez@dkfz-heidelberg.de>
2023-07-19 13:45:14 +0200

    Generate passwords only if modules are enabled

--- ccp/modules/nngm-setup.sh
+++ ccp/modules/nngm-setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 function nngmSetup() {
-	if [ -n "$NNGM_CTS_APIKEY" ]; then
-		log INFO "nNGM setup detected -- will start nNGM Connector."
-		OVERRIDE+=" -f ./$PROJECT/nngm-compose.yml"
-	fi
-	}
+  if [ -n "$NNGM_CTS_APIKEY" ]; then
+    log INFO "nNGM setup detected -- will start nNGM Connector."
+    OVERRIDE+=" -f ./$PROJECT/nngm-compose.yml"
+  fi
+}

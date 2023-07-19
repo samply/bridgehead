e8125108db6557af10e6d6508fe0e389a9297174
juarez <d.juarez@dkfz-heidelberg.de>
2023-07-19 13:45:14 +0200

    Generate passwords only if modules are enabled

--- ccp/modules/exporter-setup.sh
+++ ccp/modules/exporter-setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ -n "$ENABLE_EXPORTER" ];then
+if [ -n "$ENABLE_EXPORTER" ]; then
   log INFO "Exporter setup detected -- will start Exporter service."
   OVERRIDE+=" -f ./$PROJECT/modules/exporter-compose.yml"
+  # TODO: Generate password in another way so that not all passwords are the same?
+  EXPORTER_DB_PASSWORD="$(echo \"This is a salt string to generate one consistent password for the exporter. It is not required to be secret.\" | openssl rsautl -sign -inkey /etc/bridgehead/pki/${SITE_ID}.priv.pem | base64 | head -c 30)"
 fi
-# TODO: Generate password in another way so that not all passwords are the same?
-EXPORTER_DB_PASSWORD="$(echo \"This is a salt string to generate one consistent password for the exporter. It is not required to be secret.\" | openssl rsautl -sign -inkey /etc/bridgehead/pki/${SITE_ID}.priv.pem | base64 | head -c 30)"

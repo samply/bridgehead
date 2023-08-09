8ec387c4167f2078f5526bcfb1b4379a6b192a01
juarez <d.juarez@dkfz-heidelberg.de>
2023-08-09 18:06:32 +0200

    Generate exporter api key automatically

--- ccp/modules/exporter-setup.sh
+++ ccp/modules/exporter-setup.sh
@@ -5,4 +5,5 @@ if [ "$ENABLE_EXPORTER" == true ]; then
   OVERRIDE+=" -f ./$PROJECT/modules/exporter-compose.yml"
   # TODO: Generate password in another way so that not all passwords are the same?
   EXPORTER_DB_PASSWORD="$(echo \"This is a salt string to generate one consistent password for the exporter. It is not required to be secret.\" | openssl rsautl -sign -inkey /etc/bridgehead/pki/${SITE_ID}.priv.pem | base64 | head -c 30)"
+  EXPORTER_API_KEY="$(echo \"This is a salt string to generate one consistent API KEY for the exporter. It is not required to be secret.\" | openssl rsautl -sign -inkey /etc/bridgehead/pki/${SITE_ID}.priv.pem | base64 | head -c 64)"
 fi

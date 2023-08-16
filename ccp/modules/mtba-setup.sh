bbda5e917f25b04d436068c3c8e37057725f434d
Patrick Skowronek <patrick.skowronek@dkfz-heidelberg.de>
2023-08-16 09:35:36 +0200

    Cleanup

---
+++ ccp/modules/mtba-setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function mtbaSetup() {
+       # TODO: Check if ID-Management Module is activated!
+       if [ -n "$ENABLE_MTBA" ];then
+               log INFO "MTBA setup detected -- will start MTBA Service and CBioPortal."
+              if [ ! -n "$IDMANAGER_UPLOAD_APIKEY" ]; then
+                      log ERROR "Detected MTBA Module configuration but ID-Management Module seems not to be configured!"
+                      exit 1;
+              fi
+               OVERRIDE+=" -f ./$PROJECT/mtba-compose.yml"
+       fi
+}

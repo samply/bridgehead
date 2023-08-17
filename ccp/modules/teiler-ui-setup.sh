b2570d406c96a550c6183fd6f6459eed7cb788a1
juarez <d.juarez@dkfz-heidelberg.de>
2023-08-17 18:10:12 +0200

    Rename Teiler Backend, Teiler Dashboard and Teiler Orchestrator

--- ccp/modules/teiler-ui-setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [ "$ENABLE_TEILER" == true ];then
-  log INFO "Teiler-UI setup detected -- will start Teiler-UI services."
-  OVERRIDE+=" -f ./$PROJECT/modules/teiler-ui-compose.yml"
-fi

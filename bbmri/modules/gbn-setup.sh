6b1ea4c74e322d240a672b3b3d57ee8783a23934
patrickskowronekdkfz <86347677+patrickskowronekdkfz@users.noreply.github.com>
2023-09-04 15:54:22 +0200

    Update gbn-setup.sh

--- bbmri/modules/gbn-setup.sh
+++ bbmri/modules/gbn-setup.sh
@@ -9,5 +9,5 @@ if [ "${ENABLE_GBN}" == "true" ]; then
 	GBN_BROKER_URL=https://${GBN_BROKER_ID}
 	GBN_PROXY_ID=${SITE_ID}.${GBN_BROKER_ID}
 	GBN_FOCUS_BEAM_SECRET_SHORT="$(cat /proc/sys/kernel/random/uuid | sed 's/[-]//g' | head -c 20)"
-	GBN_SUPPORT_EMAIL=bridgehead@helpdesk.bbmri-eric.eu
+	GBN_SUPPORT_EMAIL=feedback@germanbiobanknode.de
 fi

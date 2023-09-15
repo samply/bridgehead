b5ca5ea4a78fa329a586f762ec2afcc66c38a278
lablans <m.lablans@dkfz-heidelberg.de>
2023-09-15 08:12:16 +0000

    Autogenerate maps for Opal's beam-connect. To be completed by @Threated with a map-generator in the script.

--- ccp/modules/datashield-setup.sh
+++ ccp/modules/datashield-setup.sh
@@ -5,10 +5,11 @@ if [ "$ENABLE_DATASHIELD" == true ]; then
   OVERRIDE+=" -f ./$PROJECT/modules/datashield-compose.yml"
   OPAL_DB_PASSWORD="$(echo \"This is a salt string to generate one consistent password for Opal. It is not required to be secret.\" | openssl rsautl -sign -inkey /etc/bridgehead/pki/${SITE_ID}.priv.pem | base64 | head -c 30)"
   DATASHIELD_CONNECT_SECRET="$(echo \"This is a salt string to generate one consistent password as the DataShield Connect secret. It is not required to be secret.\" | openssl rsautl -sign -inkey /etc/bridgehead/pki/${SITE_ID}.priv.pem | base64 | head -c 30)"
-  if [ ! -e "/etc/bridgehead/trusted-ca-certs/opal-cert.pem" ]; then
-    openssl req -x509 -newkey rsa:4096 -nodes -keyout /etc/bridgehead/trusted-ca-certs/opal-key.pem -out /etc/bridgehead/trusted-ca-certs/opal-cert.pem -days 3650 -subj "/CN=${HOST:-opal}/C=DE"
-    chmod g+r /etc/bridgehead/trusted-ca-certs/opal-key.pem
-    chown bridgehead:docker /etc/bridgehead/trusted-ca-certs/opal-key.pem
-    chown bridgehead:docker /etc/bridgehead/trusted-ca-certs/opal-cert.pem
+  if [ ! -e /tmp/bridgehead/opal-cert.pem ]; then
+    mkdir -p /tmp/bridgehead/
+    chown -R bridgehead:docker /tmp/bridgehead/
+    openssl req -x509 -newkey rsa:4096 -nodes -keyout /tmp/bridgehead/opal-key.pem -out /tmp/bridgehead/opal-cert.pem -days 3650 -subj "/CN=${HOST:-opal}/C=DE"
+    chown -R bridgehead:docker /tmp/bridgehead/
+    chmod g+r /tmp/bridgehead/opal-key.pem
   fi
 fi

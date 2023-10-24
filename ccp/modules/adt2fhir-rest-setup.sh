0555786435bc68786f6112fd4807797c7f6af85e
p.delpy@dkfz-heidelberg.de <p.delpy@dkfz-heidelberg.de>
2023-10-24 07:41:17 +0200

    fix bash logic

--- ccp/modules/adt2fhir-rest-setup.sh
+++ ccp/modules/adt2fhir-rest-setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-if [ -n "$ENABLE_ADT2FHIR_REST" ]; then
-  log INFO "ADT2FHIR-REST setup detected -- will start adt2fhir-rest API."
-  if [ ! -n "$IDMANAGER_LOCAL_PATIENTLIST_APIKEY" ]; then
-    log ERROR "Missing ID-Management Module! Fix this by setting up ID Management:"
-    exit 1;
+function adt2fhirRestSetup() {
+  if [ -n "$ENABLE_ADT2FHIR_REST" ]; then
+    log INFO "ADT2FHIR-REST setup detected -- will start adt2fhir-rest API."
+    if [ ! -n "$IDMANAGER_LOCAL_PATIENTLIST_APIKEY" ]; then
+      log ERROR "Missing ID-Management Module! Fix this by setting up ID Management:"
+      exit 1;
+    fi
+    OVERRIDE+=" -f ./$PROJECT/modules/adt2fhir-rest-compose.yml"
+    LOCAL_SALT="$(echo \"local-random-salt\" | openssl pkeyutl -sign -inkey /etc/bridgehead/pki/${SITE_ID}.priv.pem | base64 | head -c 30)"
   fi
-  OVERRIDE+=" -f ./$PROJECT/modules/adt2fhir-rest-compose.yml"
-  LOCAL_SALT="$(echo \"local-random-salt\" | openssl pkeyutl -sign -inkey /etc/bridgehead/pki/${SITE_ID}.priv.pem | base64 | head -c 30)"
-fi
+}
--- ccp/modules/nngm-setup.sh
+++ ccp/modules/nngm-setup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-function nngmSetup() {
-	if [ -n "$NNGM_CTS_APIKEY" ]; then
-		log INFO "nNGM setup detected -- will start nNGM Connector."
-		OVERRIDE+=" -f ./$PROJECT/modules/nngm-compose.yml"
-	fi
-}
+if [ -n "$NNGM_CTS_APIKEY" ]; then
+  log INFO "nNGM setup detected -- will start nNGM Connector."
+  OVERRIDE+=" -f ./$PROJECT/modules/nngm-compose.yml"
+fi

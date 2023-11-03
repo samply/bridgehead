2252504d7871e24cb426d388eb89763c9c9092f2
Tobias Kussel <tobias.kussel@dkfz-heidelberg.de>
2023-11-03 07:33:16 +0000

    add bwhc node module

---
+++ ccp/modules/dnpm-node-setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ -n "${ENABLE_DNPM_NODE}" ]; then
+	log INFO "DNPM setup detected (BwHC Node) -- will start BwHC node."
+	OVERRIDE+=" -f ./$PROJECT/modules/dnpm-node-compose.yml"
+
+	# Set variables required for BwHC Node. ZPM_SITE is assumed to be set in /etc/bridgehead/<project>.conf
+	if [ -z "${ZPM_SITE+x}" ]; then
+		log ERROR "Mandatory variable ZPM_SITE not defined!"
+		exit 1
+	fi
+	if [ -z "${DNPM_DATA_DIR+x}" ]; then
+		log ERROR "Mandatory variable DNPM_DATA_DIR not defined!"
+		exit 1
+	fi
+			if grep -q 'traefik.http.routers.landing.rule=PathPrefix(`/dashboard`)' /srv/docker/bridgehead/minimal/docker-compose.override.yml 2>/dev/null; then
+				echo "Override of landing page url already in place"
+			else
+				echo "Adding override of landing page url"
+				if [ -f /srv/docker/bridgehead/minimal/docker-compose.override.yml ]; then
+					echo -e '  landing:\n    labels:\n      - "traefik.http.routers.landing.rule=PathPrefix(`/dashboard`)"' >> /srv/docker/bridgehead/minimal/docker-compose.override.yml
+				else
+					echo -e 'version: "3.7"\nservices:\n  landing:\n    labels:\n      - "traefik.http.routers.landing.rule=PathPrefix(`/dashboard`)"' >> /srv/docker/bridgehead/minimal/docker-compose.override.yml
+				fi
+			fi
+fi

b28fe044d629cf1e13d6a3736bbf6b2132e773f0
janskiba <jan.skiba@dkfz-heidelberg.de>
2023-11-20 11:34:18 +0000

    Generate addtional redirect url

--- ccp/modules/teiler-setup.sh
+++ ccp/modules/teiler-setup.sh
@@ -3,5 +3,10 @@
 if [ "$ENABLE_TEILER" == true ];then
   log INFO "Teiler setup detected -- will start Teiler services."
   OVERRIDE+=" -f ./$PROJECT/modules/teiler-compose.yml"
-  generate_public_oidc_client "OIDC_PUBLIC" "https://${HOST}/ccp-teiler/*"
+  redirect_urls="https://${HOST}/ccp-teiler/*"
+  host_without_proxy="$(echo "$HOST" | cut -d '.' -f1)"
+  if [[ "$HOST" != "$host_without_proxy" ]]; then
+    redirect_urls+=",https://$host_without_proxy/ccp-teiler/*"
+  fi
+  generate_public_oidc_client "OIDC_PUBLIC" "$redirect_urls"
 fi

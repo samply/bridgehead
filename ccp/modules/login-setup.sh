163650f592da0e4fdf5e949ce18be951e551c51c
juarez <d.juarez@dkfz-heidelberg.de>
2023-11-23 15:54:44 +0100

    Add generate_password function

--- ccp/modules/login-setup.sh
+++ ccp/modules/login-setup.sh
@@ -3,5 +3,5 @@
 if [ "$ENABLE_LOGIN" == true ]; then
   log INFO "Login setup detected -- will start Login services."
   OVERRIDE+=" -f ./$PROJECT/modules/login-compose.yml"
-  KEYCLOAK_DB_PASSWORD="$(echo \"This is a salt string to generate one consistent password for Keycloak. It is not required to be secret.\" | openssl rsautl -sign -inkey /etc/bridgehead/pki/${SITE_ID}.priv.pem | base64 | head -c 30)"
+  KEYCLOAK_DB_PASSWORD="$(generate_password \"local Keycloak\")"
 fi

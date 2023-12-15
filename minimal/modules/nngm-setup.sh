fb6af1c4af3b7db363899fe13e6645598142596c
Tobias Kussel <tobias.kussel@dkfz-heidelberg.de>
2023-12-15 09:43:31 +0000

    Add nngm module to minimal project

---
+++ minimal/modules/nngm-setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+function nngmSetup() {
+	if [ -n "$NNGM_CTS_APIKEY" ]; then
+		log INFO "nNGM setup detected -- will start nNGM Connector."
+		OVERRIDE+=" -f ./$PROJECT/modules/nngm-compose.yml"
+	fi
+	}

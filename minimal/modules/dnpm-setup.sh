c02da838c71985bf3697994efe660da11fa7221f
Tobias Kussel <tobias.kussel@dkfz-heidelberg.de>
2023-12-15 09:41:11 +0000

    Cleanup dnpm connect module

--- minimal/modules/dnpm-setup.sh
+++ minimal/modules/dnpm-setup.sh
@@ -5,9 +5,12 @@ if [ -n "${ENABLE_DNPM}" ]; then
 	OVERRIDE+=" -f ./$PROJECT/modules/dnpm-compose.yml"
 
 	# Set variables required for Beam-Connect
-	DNPM_APPLICATION_SECRET="$(echo \"This is a salt string to generate one consistent password for DNPM. It is not required to be secret.\" | openssl pkeyutl -sign -inkey /etc/bridgehead/pki/${SITE_ID}.priv.pem | base64 | head -c 30)"
 	DNPM_BEAM_SECRET_SHORT="$(cat /proc/sys/kernel/random/uuid | sed 's/[-]//g' | head -c 20)"
 	DNPM_BROKER_ID="broker.ccp-it.dktk.dkfz.de"
 	DNPM_BROKER_URL="https://${DNPM_BROKER_ID}"
+	if [ -z ${BROKER_URL_FOR_PREREQ+x} ]; then
+		BROKER_URL_FOR_PREREQ=$DNPM_BROKER_URL
+		log DEBUG "No Broker for clock check set; using $DNPM_BROKER_URL"
+	fi
 	DNPM_PROXY_ID="${SITE_ID}.${DNPM_BROKER_ID}"
 fi

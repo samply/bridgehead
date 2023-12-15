66d462997822159a1bd250428ca0492bf0a5982f
root <root@e260-nb015.inet.dkfz-heidelberg.de>
2023-12-15 12:30:14 +0100

    Integrating cbioportal

--- ccp/modules/cbioportal-setup.sh
+++ ccp/modules/cbioportal-setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-if [ "$ENABLE_CBIOPORTAL" == true ]; then
+# if [ "$ENABLE_CBIOPORTAL" == true ]; then
   log INFO "cBioPortal setup detected -- will start cBioPortal service."
   OVERRIDE+=" -f ./$PROJECT/modules/cbioportal-compose.yml"
   CBIOPORTAL_DB_PASSWORD="$(echo \"This is a salt string to generate one consistent password for the cbioportal database. It is not required to be secret.\" | openssl rsautl -sign -inkey /etc/bridgehead/pki/${SITE_ID}.priv.pem | base64 | head -c 30)"
-  CBIOPORTAL_DB_ROOT_PASSWORD="$(echo \"This is a salt string to generate one consistent root password for the cbioportal database. It is not required to be secret.\" | openssl rsautl -sign -inkey /etc/bridgehead/pki/${SITE_ID}.priv.pem | base64 | head -c 64)"
-fi
+  CBIOPORTAL_DB_ROOT_PASSWORD="$(echo \"This is a salt string to generate one consistent root password for the cbioportal database. It is not required to be secret.\" | openssl rsautl -sign -inkey /etc/bridgehead/pki/${SITE_ID}.priv.pem | base64 | head -c 64)" 
+# fi

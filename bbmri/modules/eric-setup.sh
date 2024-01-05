954d46efb1f98b978d706ba295d2eed76f9ee41b
Patrick Skowronek <patrick.skowronek@dkfz-heidelberg.de>
2024-01-05 11:58:42 +0100

    Added test root certs and logic for beam to use test brokers

--- bbmri/modules/eric-setup.sh
+++ bbmri/modules/eric-setup.sh
@@ -6,6 +6,13 @@ if [ "${ENABLE_ERIC}" == "true" ]; then
 
 	# Set required variables
 	ERIC_BROKER_ID=broker.bbmri.samply.de
+	ERIC_ROOT_CERT=eric
+
+	if [ "{$ENABLE_TEST}" == "true" ]; then
+		ERIC_BROKER_ID=broker-test.bbmri-test.samply.de
+		ERIC_ROOT_CERT=eric.test
+	fi
+	
 	ERIC_BROKER_URL=https://${ERIC_BROKER_ID}
 	ERIC_PROXY_ID=${SITE_ID}.${ERIC_BROKER_ID}
 	ERIC_FOCUS_BEAM_SECRET_SHORT="$(cat /proc/sys/kernel/random/uuid | sed 's/[-]//g' | head -c 20)"
--- bbmri/modules/gbn-setup.sh
+++ bbmri/modules/gbn-setup.sh
@@ -6,6 +6,12 @@ if [ "${ENABLE_GBN}" == "true" ]; then
 
 	# Set required variables
 	GBN_BROKER_ID=broker.bbmri.de
+	GBN_ROOT_CERT=gbn
+	if [ "{$ENABLE_TEST}" == "true" ]; then
+		GBN_BROKER_ID=broker-test.bbmri.de
+		GBN_ROOT_CERT=gbn.test
+	fi
+	
 	GBN_BROKER_URL=https://${GBN_BROKER_ID}
 	GBN_PROXY_ID=${SITE_ID}.${GBN_BROKER_ID}
 	GBN_FOCUS_BEAM_SECRET_SHORT="$(cat /proc/sys/kernel/random/uuid | sed 's/[-]//g' | head -c 20)"

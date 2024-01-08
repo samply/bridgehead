2074461ee770fd7199fa9d4ced41ca2fefaf2df9
Martin Lablans <m.lablans@dkfz-heidelberg.de>
2024-01-08 13:03:12 +0100

    Use new variable ENVIRONMENT in /etc/bridgehead; defaults to "production".

--- bbmri/modules/eric-setup.sh
+++ bbmri/modules/eric-setup.sh
@@ -4,15 +4,23 @@ if [ "${ENABLE_ERIC}" == "true" ]; then
 	log INFO "BBMRI-ERIC setup detected -- will start services for BBMRI-ERIC."
 	OVERRIDE+=" -f ./$PROJECT/modules/eric-compose.yml"
 
-	# Set required variables
-	ERIC_BROKER_ID=broker.bbmri.samply.de
-	ERIC_ROOT_CERT=eric
+	# The environment needs to be defined in /etc/bridgehead
+	case "$ENVIRONMENT" in
+		"production")
+			ERIC_BROKER_ID=broker.bbmri.samply.de
+			ERIC_ROOT_CERT=eric
+			;;
+		"test")
+			ERIC_BROKER_ID=broker-test.bbmri-test.samply.de
+			ERIC_ROOT_CERT=eric.test
+			;;
+		*)
+			report_error 6 "Environment \"$ENVIRONMENT\" is unknown. Assuming production. FIX THIS!"
+			ERIC_BROKER_ID=broker.bbmri.samply.de
+			ERIC_ROOT_CERT=eric
+			;;
+	esac
 
-	if [ "{$ENABLE_TEST}" == "true" ]; then
-		ERIC_BROKER_ID=broker-test.bbmri-test.samply.de
-		ERIC_ROOT_CERT=eric.test
-	fi
-	
 	ERIC_BROKER_URL=https://${ERIC_BROKER_ID}
 	ERIC_PROXY_ID=${SITE_ID}.${ERIC_BROKER_ID}
 	ERIC_FOCUS_BEAM_SECRET_SHORT="$(cat /proc/sys/kernel/random/uuid | sed 's/[-]//g' | head -c 20)"

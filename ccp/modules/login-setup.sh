40d4c10e11ab4fd2d1953e3015c40a9535f3e52e
juarez <d.juarez@dkfz-heidelberg.de>
2024-02-13 15:18:37 +0100

    Removed:Login-compose

--- ccp/modules/login-setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-if [ "$ENABLE_LOGIN" == true ]; then
-  log INFO "Login setup detected -- will start Login services."
-  OVERRIDE+=" -f ./$PROJECT/modules/login-compose.yml"
-  KEYCLOAK_DB_PASSWORD="$(generate_password \"local Keycloak\")"
-fi

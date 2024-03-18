480bbe04e75ad9453815a88ae6296862786f6f46
djuarezgf <d.juarez@dkfz-heidelberg.de>
2024-03-18 16:47:40 +0100

    Changed: TEILER_DEFAULT_LANGUAGE

--- ccp/modules/teiler-setup.sh
+++ ccp/modules/teiler-setup.sh
@@ -3,7 +3,7 @@
 if [ "$ENABLE_TEILER" == true ];then
   log INFO "Teiler setup detected -- will start Teiler services."
   OVERRIDE+=" -f ./$PROJECT/modules/teiler-compose.yml"
-  DEFAULT_LANGUAGE=DE
-  DEFAULT_LANGUAGE_LOWER_CASE=${DEFAULT_LANGUAGE,,}
+  TEILER_DEFAULT_LANGUAGE=DE
+  TEILER_DEFAULT_LANGUAGE_LOWER_CASE=${TEILER_DEFAULT_LANGUAGE,,}
   add_public_oidc_redirect_url "/ccp-teiler/*"
 fi

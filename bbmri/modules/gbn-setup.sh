1afbf88a76ed190c082eb0f8335eb1d7f2db905c
Patrick Skowronek <patrick.skowronek@dkfz-heidelberg.de>
2024-04-16 09:23:42 +0200

    fix: use only bbmri broker

--- bbmri/modules/gbn-setup.sh
+++ bbmri/modules/gbn-setup.sh
@@ -21,7 +21,7 @@ if [ "${ENABLE_GBN}" == "true" ]; then
 			;;
 	esac
 	
-	GBN_BROKER_URL=https://${GBN_BROKER_ID}
+	#GBN_BROKER_URL=https://${GBN_BROKER_ID}
 	GBN_PROXY_ID=${SITE_ID}.${GBN_BROKER_ID}
 	GBN_FOCUS_BEAM_SECRET_SHORT="$(cat /proc/sys/kernel/random/uuid | sed 's/[-]//g' | head -c 20)"
 	GBN_SUPPORT_EMAIL=feedback@germanbiobanknode.de

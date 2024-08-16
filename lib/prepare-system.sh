95ec73f5f2cdc805eabf60e988a521612097a501
Torben Brenner <t.brenner@dkfz-heidelberg.de>
2024-08-16 12:18:22 +0200

    feat: added dhki project

--- lib/prepare-system.sh
+++ lib/prepare-system.sh
@@ -57,6 +57,9 @@ case "$PROJECT" in
 		;;
 	itcc)
 		site_configuration_repository_middle="git.verbis.dkfz.de/itcc-sites/"
+        ;;
+    dhki)
+		site_configuration_repository_middle="git.verbis.dkfz.de/dhki/"
 		;;
 	minimal)
 		site_configuration_repository_middle="git.verbis.dkfz.de/minimal-bridgehead-configs/"

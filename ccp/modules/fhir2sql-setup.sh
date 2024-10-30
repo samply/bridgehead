64915eb018b74a55cbb046c3705cc1c304a1ce30
davidmscholz <david.scholz@dkfz-heidelberg.de>
2024-10-30 12:30:39 +0100

    add and set FOCUS_ENDPOINT_TYPE

--- ccp/modules/fhir2sql-setup.sh
+++ ccp/modules/fhir2sql-setup.sh
@@ -4,4 +4,5 @@ if [ "$ENABLE_FHIR2SQL" == true ]; then
   log INFO "Dashboard setup detected -- will start Dashboard backend and FHIR2SQL service."
   OVERRIDE+=" -f ./$PROJECT/modules/fhir2sql-compose.yml"
   DASHBOARD_DB_PASSWORD="$(generate_simple_password 'fhir2sql')"
+  export FOCUS_ENDPOINT_TYPE="blaze-and-sql"
 fi

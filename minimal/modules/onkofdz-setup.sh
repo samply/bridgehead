2aad2ca9bf479d16e3d8d01e8adb8303f7cac5a1
Tobias Kussel <tobias.kussel@dkfz-heidelberg.de>
2024-10-31 11:44:15 +0000

    Use onkofdz specific broker

--- minimal/modules/onkofdz-setup.sh
+++ minimal/modules/onkofdz-setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ -n "${ENABLE_ONKOFDZ}" ]; then
-  BROKER_ID=test-no-real-data.broker.samply.de
+  BROKER_ID=test.broker.onkofdz.samply.de
   BROKER_URL=https://${BROKER_ID}
   PROXY_ID=${SITE_ID}.${BROKER_ID}
   BEAMSEL_SECRET="$(cat /proc/sys/kernel/random/uuid | sed 's/[-]//g' | head -c 20)"
-  SUPPORT_EMAIL=support-ccp@dkfz-heidelberg.de
+  SUPPORT_EMAIL=tobias.kussel@dkfz-heidelberg.de
   PRIVATEKEYFILENAME=/etc/bridgehead/pki/${SITE_ID}.priv.pem
 
   BROKER_URL_FOR_PREREQ=$BROKER_URL

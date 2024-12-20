ba31975887687420b9f00db7692990b9601ed3ba
Martin Lablans <m.lablans@dkfz-heidelberg.de>
2024-12-20 12:00:29 +0100

    Add gitCredentials to bridgehead script

--- lib/functions.sh
+++ lib/functions.sh
@@ -53,7 +53,7 @@ checkOwner(){
 }
 
 printUsage() {
-	echo "Usage: bridgehead start|stop|logs|docker-logs|is-running|update|install|uninstall|adduser|enroll PROJECTNAME"
+	echo "Usage: bridgehead start|stop|logs|docker-logs|is-running|update|install|uninstall|adduser|enroll|gitCredentials PROJECTNAME"
 	echo "PROJECTNAME should be one of ccp|bbmri|cce|itcc|kr|dhki"
 }
 

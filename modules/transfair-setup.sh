139fcecabe2825ad3c253b4565baae92f4d01459
janskiba <jan.skiba@dkfz-heidelberg.de>
2025-01-23 12:42:59 +0000

    redo transfair setup

--- modules/transfair-setup.sh
+++ modules/transfair-setup.sh
@@ -1,13 +1,24 @@
 #!/bin/bash -e
 
 function transfairSetup() {
-	assertVarsNotEmpty INSTITUTE_TTP_URL INSTITUTE_TTP_API_KEY PROJECT_ID_SYSTEM FHIR_REQUEST_URL FHIR_INPUT_URL
+    if [[ -n "$INSTITUTE_TTP_URL" || -n "$EXCHANGE_ID_SYSTEM" ]]; then
+        echo "Starting transfair."
+    else
+        return
+    fi
 	OVERRIDE+=" -f ./modules/transfair-compose.yml"
-	if [ -n "$FHIR_OUTPUT_URL" ]; then
-		log INFO "TransFAIR output fhir store set to external $FHIR_OUTPUT_URL"
+	if [ -n "$FHIR_INPUT_URL" ]; then
+		log INFO "TransFAIR input fhir store set to external $FHIR_INPUT_URL"
 	else
-		log INFO "TransFAIR output fhir store not set writing to internal blaze"
-		FHIR_OUTPUT_URL="http://transfair-blaze:8080"
-		OVERRIDE+=" -f ./modules/transfair-compose.yml"
+		log INFO "TransFAIR input fhir store not set writing to internal blaze"
+		FHIR_INPUT_URL="http://bridgehead-transfair-input-blaze:8080"
+		OVERRIDE+=" --profile transfair-input-blaze"
+	fi
+	if [ -n "$FHIR_REQUEST_URL" ]; then
+		log INFO "TransFAIR request fhir store set to external $FHIR_REQUEST_URL"
+	else
+		log INFO "TransFAIR request fhir store not set writing to internal blaze"
+		FHIR_REQUEST_URL="http://bridgehead-transfair-requests-blaze:8080"
+		OVERRIDE+=" --profile transfair-request-blaze"
 	fi
 }

c182cbe9e5d9040dd13449d1f2dd1fa620da6253
janskiba <jan.skiba@dkfz-heidelberg.de>
2025-01-29 09:47:55 +0000

    fix: properly load oidc secrets

--- lib/functions.sh
+++ lib/functions.sh
@@ -318,7 +318,7 @@ function sync_secrets() {
         docker.verbis.dkfz.de/cache/samply/secret-sync-local:latest
 
     set -a # Export variables as environment variables
-    source /var/cache/bridgehead/secrets/*
+    source /var/cache/bridgehead/secrets/oidc
     set +a # Export variables in the regular way
 }
 

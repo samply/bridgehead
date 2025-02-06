146235236b41b04277f481c4ebd4ffdbc10f88df
Gerhard Salvini <gerhard@ubuntu.com>
2025-02-06 09:28:48 +0100

    Removed stuff accumulated during testing phase
    Most of the things added during testing were not necessary and they were
    removed. This had the additional advantage that many files are now identical
    to their equivalents in the develop branch, making the diff more manageable.

--- bbmri/modules/feedback-agent-setup.sh
+++ bbmri/modules/feedback-agent-setup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-log INFO "######################################### Metadata feedback script was found by Bridgehead"
-
 if [ "$ENABLE_FEEDBACK_AGENT" == true ]; then
-  log INFO "######################################### Metadata feedback setup detected -- will start Feedback service."
   OVERRIDE+=" -f ./$PROJECT/modules/feedback-agent-compose.yml"
   FEEDBACK_AGENT_BEAM_SECRET="$(cat /proc/sys/kernel/random/uuid | sed 's/[-]//g' | head -c 20)"
   FEEDBACK_AGENT_DB_PASSWORD="$(cat /proc/sys/kernel/random/uuid | sed 's/[-]//g' | head -c 20)"

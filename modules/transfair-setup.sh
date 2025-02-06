bca63e82a9151272987521faca51284629d8fcf5
Torben Brenner <t.brenner@dkfz-heidelberg.de>
2025-02-06 15:43:37 +0100

    fix: don't use return in transfairSetup
    For some reason the return not only exits transfairSetup, but also the
    bridgehead script

--- modules/transfair-setup.sh
+++ modules/transfair-setup.sh
@@ -3,22 +3,20 @@
 function transfairSetup() {
     if [[ -n "$INSTITUTE_TTP_URL" || -n "$EXCHANGE_ID_SYSTEM" ]]; then
         echo "Starting transfair."
-    else
-        return
+	    OVERRIDE+=" -f ./modules/transfair-compose.yml"
+	    if [ -n "$FHIR_INPUT_URL" ]; then
+		    log INFO "TransFAIR input fhir store set to external $FHIR_INPUT_URL"
+	    else
+		    log INFO "TransFAIR input fhir store not set writing to internal blaze"
+		    FHIR_INPUT_URL="http://bridgehead-transfair-input-blaze:8080"
+		    OVERRIDE+=" --profile transfair-input-blaze"
+	    fi
+	    if [ -n "$FHIR_REQUEST_URL" ]; then
+		    log INFO "TransFAIR request fhir store set to external $FHIR_REQUEST_URL"
+	    else
+		    log INFO "TransFAIR request fhir store not set writing to internal blaze"
+		    FHIR_REQUEST_URL="http://bridgehead-transfair-requests-blaze:8080"
+		    OVERRIDE+=" --profile transfair-request-blaze"
+	    fi
     fi
-	OVERRIDE+=" -f ./modules/transfair-compose.yml"
-	if [ -n "$FHIR_INPUT_URL" ]; then
-		log INFO "TransFAIR input fhir store set to external $FHIR_INPUT_URL"
-	else
-		log INFO "TransFAIR input fhir store not set writing to internal blaze"
-		FHIR_INPUT_URL="http://bridgehead-transfair-input-blaze:8080"
-		OVERRIDE+=" --profile transfair-input-blaze"
-	fi
-	if [ -n "$FHIR_REQUEST_URL" ]; then
-		log INFO "TransFAIR request fhir store set to external $FHIR_REQUEST_URL"
-	else
-		log INFO "TransFAIR request fhir store not set writing to internal blaze"
-		FHIR_REQUEST_URL="http://bridgehead-transfair-requests-blaze:8080"
-		OVERRIDE+=" --profile transfair-request-blaze"
-	fi
 }

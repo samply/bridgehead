8384143387a48e84d8c5e2e8dcb171dbed9416a8
Torben Brenner <t.brenner@dkfz-heidelberg.de>
2025-02-07 09:17:17 +0100

    fix: make transfair reach the internal blaze stores

--- modules/transfair-setup.sh
+++ modules/transfair-setup.sh
@@ -8,14 +8,14 @@ function transfairSetup() {
 		    log INFO "TransFAIR input fhir store set to external $FHIR_INPUT_URL"
 	    else
 		    log INFO "TransFAIR input fhir store not set writing to internal blaze"
-		    FHIR_INPUT_URL="http://bridgehead-transfair-input-blaze:8080"
+		    FHIR_INPUT_URL="http://transfair-input-blaze:8080"
 		    OVERRIDE+=" --profile transfair-input-blaze"
 	    fi
 	    if [ -n "$FHIR_REQUEST_URL" ]; then
 		    log INFO "TransFAIR request fhir store set to external $FHIR_REQUEST_URL"
 	    else
 		    log INFO "TransFAIR request fhir store not set writing to internal blaze"
-		    FHIR_REQUEST_URL="http://bridgehead-transfair-requests-blaze:8080"
+		    FHIR_REQUEST_URL="http://transfair-requests-blaze:8080"
 		    OVERRIDE+=" --profile transfair-request-blaze"
 	    fi
     fi

1f37777e6aa50137213f9b0f3839a7ecdaab311d
Tobias Kussel <tobias.kussel@dkfz-heidelberg.de>
2025-03-11 10:59:00 +0100

    Added ccp module for a ssh tunnel
    Usage details under https://github.com/samply/ssh-tunnel

---
+++ ccp/modules/ssh-tunnel-setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+  if [ -n "$ENABLE_SSH_TUNNEL" ]; then
+    log INFO "SSH Tunnel setup detected -- will start SSH Tunnel."
+    OVERRIDE+=" -f ./$PROJECT/modules/ssh-tunnel-compose.yml"
+  fi

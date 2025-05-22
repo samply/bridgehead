d068b1c6df4507f93cc6398e7a9913d2242fcf76
David Juarez <d.juarez@dkfz-heidelberg.de>
2025-05-22 14:30:30 +0200

    Teiler in BBMRI

--- ccp/modules/teiler-setup.sh
+++ ccp/modules/teiler-setup.sh
@@ -3,7 +3,5 @@
 if [ "$ENABLE_TEILER" == true ];then
   log INFO "Teiler setup detected -- will start Teiler services."
   OVERRIDE+=" -f ./ccp/modules/teiler-compose.yml"
-  TEILER_DEFAULT_LANGUAGE=DE
-  TEILER_DEFAULT_LANGUAGE_LOWER_CASE=${TEILER_DEFAULT_LANGUAGE,,}
   add_public_oidc_redirect_url "/ccp-teiler/*"
 fi

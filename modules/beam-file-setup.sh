24dec5a1725adad1de5b0a86f6c08988499dceb3
Torben Brenner <t.brenner@dkfz-heidelberg.de>
2025-05-23 16:21:10 +0200

    refactor: beam file send is only an executable command

--- modules/beam-file-setup.sh
+++ modules/beam-file-setup.sh
@@ -1,14 +1,7 @@
 #!/bin/bash -e
 
 function beamFileSetup() {
-    if [ -n "$ENABLE_BEAM_FILE_SENDER" ]; then
-       echo "Starting beam file in sender mode"
-       OVERRIDE+=" -f ./modules/beam-file-compose.yml --profile beam-file-sender"
-       BEAM_FILE_SECRET="$(cat /proc/sys/kernel/random/uuid | sed 's/[-]//g' | head -c 20)"
-       # NOTE: We could make this persistent across restarts
-       BEAM_FILE_API_KEY="$(cat /proc/sys/kernel/random/uuid | sed 's/[-]//g' | head -c 20)"
-       log INFO "Beam File in Sender Mode available uses ApiKey ${BEAM_FILE_API_KEY}"
-    elif [ -n "$ENABLE_BEAM_FILE_RECEIVER" ]; then
+    if [ -n "$ENABLE_BEAM_FILE_RECEIVER" ]; then
        echo "Starting beam file in receiver mode"
        OVERRIDE+=" -f ./modules/beam-file-compose.yml --profile beam-file-receiver"
        BEAM_FILE_SECRET="$(cat /proc/sys/kernel/random/uuid | sed 's/[-]//g' | head -c 20)"

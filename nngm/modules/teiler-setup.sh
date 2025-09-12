e853667d9dc4d4ded5577bfd72bfb0abc0ed1399
David Juarez <d.juarez@dkfz-heidelberg.de>
2025-09-12 14:32:45 +0200

    feat: nNGM project

---
+++ nngm/modules/teiler-setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+if [ "$ENABLE_TEILER" == true ];then
+  log INFO "Teiler setup detected -- will start Teiler services."
+  OVERRIDE+=" -f ./$PROJECT/modules/teiler-compose.yml"
+  TEILER_DEFAULT_LANGUAGE=DE
+  TEILER_DEFAULT_LANGUAGE_LOWER_CASE=${TEILER_DEFAULT_LANGUAGE,,}
+fi

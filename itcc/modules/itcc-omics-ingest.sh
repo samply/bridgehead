017620e87018777e8f782bb96d7c8226b692039a
Martin Jurk <martin.jurk@dkfz-heidelberg.de>
2026-02-05 16:29:38 +0100

    mainzelliste test

--- itcc/modules/itcc-omics-ingest.sh
+++ itcc/modules/itcc-omics-ingest.sh
@@ -3,4 +3,5 @@
 if [ -n "$ENABLE_OMICS" ];then
   OVERRIDE+=" -f ./$PROJECT/modules/itcc-omics-ingest.yaml"
   GENERATE_API_KEY="$(generate_simple_password 'omics')"
+  PATIENTLIST_POSTGRES_PASSWORD=="$(generate_simple_password 'mainzelliste')"
 fi

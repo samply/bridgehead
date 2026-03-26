b747a1baf6ab93298753125696684da238ab6751
Tobias Kussel <tobias.kussel@dkfz-heidelberg.de>
2026-03-26 13:25:47 +0100

    Add ovis oidc redirect path

--- ccp/modules/ovis-setup.sh
+++ ccp/modules/ovis-setup.sh
@@ -3,4 +3,5 @@
 if [ -n "$ENABLE_OVIS" ]; then
   log INFO "OVIS setup detected -- will start OVIS services with local oauth2-proxy middleware."
   OVERRIDE+=" -f ./$PROJECT/modules/ovis-compose.yml"
+  add_private_oidc_redirect_url "/oauth2-ovis/callback"
 fi

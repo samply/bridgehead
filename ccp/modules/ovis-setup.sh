55d629c343f65db7d213a08c429b23a3e9fed39e
tm16-medma <thewind.mom@medma.uni-heidelberg.de>
2026-03-26 09:04:42 +0100

    Add oauth2-proxy middleware for OVIS services in ovis-compose.yml
    Introduced a new service, `ovis-traefik-forward-auth`, to handle authentication via oauth2-proxy. Updated Traefik routing for existing services to integrate the new middleware, ensuring secure access control. Adjusted logging in ovis-setup.sh to reflect the addition of the oauth2-proxy middleware in the OVIS setup process.

--- ccp/modules/ovis-setup.sh
+++ ccp/modules/ovis-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
 if [ -n "$ENABLE_OVIS" ]; then
-  log INFO "OVIS setup detected -- will start OVIS services."
+  log INFO "OVIS setup detected -- will start OVIS services with local oauth2-proxy middleware."
   OVERRIDE+=" -f ./$PROJECT/modules/ovis-compose.yml"
 fi

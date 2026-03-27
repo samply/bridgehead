9ca6d0f17888af94d12dc025b0a313645782504e
Skiba Jan <jan.skiba@dkfz-heidelberg.de>
2026-03-27 10:13:50 +0100

    auto generate ovis cookie secret

--- ccp/modules/ovis-setup.sh
+++ ccp/modules/ovis-setup.sh
@@ -4,4 +4,5 @@ if [ -n "$ENABLE_OVIS" ]; then
   log INFO "OVIS setup detected -- will start OVIS services with local oauth2-proxy middleware."
   OVERRIDE+=" -f ./$PROJECT/modules/ovis-compose.yml"
   add_private_oidc_redirect_url "/oauth2-ovis/callback"
+  OVIS_AUTH_COOKIE_SECRET="$(generate_simple_password 'ovisCookieSecret' | head -c 16)"
 fi

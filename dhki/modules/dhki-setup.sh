c7802384f2aa3be5e9dace0bf13c444860c42758
Torben Brenner <t.brenner@dkfz-heidelberg.de>
2026-06-24 16:22:55 +0200

    Prepared development environment in dhki project

---
+++ dhki/modules/dhki-setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+log INFO "DKFZ-Hector setup detected -- will start services for DKFZ-Hector."
+
+# The environment needs to be defined in /etc/bridgehead
+case "$ENVIRONMENT" in
+	"production")
+		export BROKER_ID=broker.hector.dkfz.de
+		export DHKI_ROOT_CERT=dhki
+		;;
+	"development")
+		export BROKER_ID=dev-dhki-broker.vms.verbis.dkfz.de
+		export DHKI_ROOT_CERT=dhki.dev
+		;;
+	*)
+		report_error 6 "Environment \"$ENVIRONMENT\" is unknown. Assuming production. FIX THIS!"
+		export BROKER_ID=broker.hector.dkfz.de
+		export DHKI_ROOT_CERT=dhki
+		;;
+esac

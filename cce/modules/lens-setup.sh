5e07fc3e83f136bdcb0a75a663fe9eec293f39e4
David Juarez <d.juarez@dkfz-heidelberg.de>
2026-07-29 13:21:37 +0200

    chore: oauth2 in lens

--- cce/modules/lens-setup.sh
+++ cce/modules/lens-setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 if [ -n "$ENABLE_LENS" ];then
+  LENS_AUTH_COOKIE_SECRET="$(echo "This is a salt string to generate one consistent cookie secret for the Lens oauth2 proxy." | sha1sum | openssl pkeyutl -sign -inkey /etc/bridgehead/pki/${SITE_ID}.priv.pem | base64 | tr -d '\n' | head -c 32)"
   OVERRIDE+=" -f ./$PROJECT/modules/lens-compose.yml"
 fi

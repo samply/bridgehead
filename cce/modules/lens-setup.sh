f1b0796d24108dd7b16185ef4922a8a048274cc1
David Juarez <d.juarez@dkfz-heidelberg.de>
2026-07-31 13:22:25 +0200

    fix: oidc config in project-manager and lens for cce

--- cce/modules/lens-setup.sh
+++ cce/modules/lens-setup.sh
@@ -3,4 +3,9 @@
 if [ -n "$ENABLE_LENS" ];then
   LENS_AUTH_COOKIE_SECRET="$(echo "This is a salt string to generate one consistent cookie secret for the Lens oauth2 proxy." | sha1sum | openssl pkeyutl -sign -inkey /etc/bridgehead/pki/${SITE_ID}.priv.pem | base64 | tr -d '\n' | head -c 32)"
   OVERRIDE+=" -f ./$PROJECT/modules/lens-compose.yml"
+
+  LENS_OIDC_PRIVATE_URL="https://sso.verbis.dkfz.de/application/o/cce-project-manager-api/"
+  LENS_OIDC_PRIVATE_CLIENT_ID="cce-project-manager-api"
+  LENS_OIDC_PRIVATE_SECRET="$(echo \"This is a salt string to generate the private secret for the project manager oidc public client. It is not required to be secret.\" | sha1sum | openssl pkeyutl -sign -inkey /etc/bridgehead/pki/${SITE_ID}.priv.pem | base64 | head -c 30)"
+
 fi
